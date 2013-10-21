fd8643dc0f65f194fb0eea78231d99c7eea3b5fe
MaritalWheat <rosu.emanuel@gmail.com>
2013-10-20 22:42:08 -0500

    prototype extracts date/gametype/map/rank information

--- harvester.lua
+++ harvester.lua
@@ -1,10 +1,14 @@
 local http = require("socket.http")
 local baseAddress = "http://halo.bungie.net/stats/playerstatshalo3.aspx?player=MaritalWheat&ctl00_mainContent_bnetpgl_recentgamesChangePage="
+local check = false;
+
 for addresser = 1, 5, 1 do
-	
+	local dataSegToMatch = "</td><td>";
 	local page = http.request(baseAddress .. addresser)
+
 	print ("Page: " .. addresser .. "-----------------")
 	for token in string.gmatch(page, "%g+") do
+		local check_seg = string.sub(token, 0, string.len(dataSegToMatch));
 		local first_char = string.sub(token, 0, 1)
 		if (first_char == 'h') then  --know we have a game header
 			local tokLength = string.len(token)
@@ -18,5 +22,16 @@ for addresser = 1, 5, 1 do
 				end
 			end
 		end
+		
+		if (check) then
+			print (token);
+			check = false;
+		end
+		
+		if (check_seg == dataSegToMatch) then
+			print (check_seg);
+			check = true;
+		end
+		
 	end
 end

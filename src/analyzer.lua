89f3eb10634981cc2f450b9cb4599e51a59f2511
MaritalWheat <rosu.emanuel@gmail.com>
2014-02-03 16:00:51 -0600

    month translation

--- src/analyzer.lua
+++ src/analyzer.lua
@@ -32,6 +32,49 @@ function string:split(inSplitPattern, outResults)
    return outResults
 end
 
+function month_to_int(in_string)
+	if (in_string == "January") then return 1 end;
+	if (in_string == "February") then return 2 end;
+	if (in_string == "March") then return 3 end;
+	if (in_string == "April") then return 4 end;
+	if (in_string == "May") then return 5 end;
+	if (in_string == "June") then return 6 end;
+	if (in_string == "July") then return 7 end;
+	if (in_string == "August") then return 8 end;
+	if (in_string == "September") then return 9 end;
+	if (in_string == "October") then return 10 end;
+	if (in_string == "November") then return 11 end;
+	if (in_string == "December") then return 12 end;
+end
+
+function parse_date(in_string)
+	--input format: [day] [month + date] [year]
+	local out = {month = "-", date = "-", year = "-"};
+	local input = in_string:split(", ");
+
+	if (input[2] == nil) then return out end;
+	if (input[3] == nil) then return out end;
+
+	--split month and date from single entry
+	local month_date = input[2]:split(" ");
+
+	if (month_date[1] == nil) then return out end;
+	if (month_date[2] == nil) then return out end;
+
+	local month_string = month_date[1];
+	local month = month_to_int(month_string);
+	local date = month_date[2];
+	local year = input[3];
+
+	out.month = month;
+	out.date = date;
+	out.year = year;
+
+	print(out.month);
+	print(out.date);
+	print(out.year);
+end
+
 data = get_path();
 
 local data_index = 1;
@@ -61,6 +104,7 @@ while(line ~= nil) do
 			--insert Player object into table
 			table.insert(player_table, Player);
 			print(Player.name);
+			parse_date(split_line[7]);
 			--update current player
 			curr_player = split_line[5];
 		end
